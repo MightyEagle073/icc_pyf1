5c6e8dbce7981ccfc9eb5ea1a7d46f2b7f8a3375
Oscar Liang <utuensw@gmail.com>
2024-10-22 04:15:50 +1100

    Initialisation

---
+++ Week1/printing.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+    printf("This is a preview of how printing.py is supposed to be like.\n");
+    printf("Remember, your program does not exactly have to be like this program!\n\n");
+    printf("Hello, my name is John! \n");
+    printf("I am 12 years old. \n");
+    printf("I can speak three languages fluently. \n\n");
+    printf("Press enter to exit preview");
+    getchar();
+}
---
+++ Week1/printing.py
@@ -0,0 +1,5 @@
+# ICC Python Fundamentals 1, Week 1, Exercise 1: Printing
+# By [your name] on [date]
+#
+# Your task is to write a python program to print out your name, age and an 
+# interesting fact about yourself.
